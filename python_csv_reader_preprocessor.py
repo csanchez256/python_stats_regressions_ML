744e7c83de771c1846340af0b85acb645f6e6b8a
chris <css7c3@gmail.com>
2024-07-28 21:45:30 -0600

    reading columns into a dictionary

--- python_csv_reader_preprocessor.py
+++ python_csv_reader_preprocessor.py
@@ -7,3 +7,17 @@ with open('C:\\Users\\css7c\\OneDrive\\Desktop\\Python_Programs\\nm_clean.csv', 
     reader = csv.reader(f)
     for row in reader:
         print(row)
+
+
+# https://realpython.com/python-csv/
+
+with open('C:\\Users\\css7c\\OneDrive\\Desktop\\Python_Programs\\nm_clean.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        
+        line_count += 1
+    print(f'Processed {line_count} lines.')        
