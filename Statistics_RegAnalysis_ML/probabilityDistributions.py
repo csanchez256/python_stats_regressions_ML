30e0d5428924f3d8b48f3de86ed566400480f330
chris <css7c3@gmail.com>
2024-08-08 10:55:16 -0600

    first commit

---
+++ Statistics_RegAnalysis_ML/probabilityDistributions.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+
+education_districtwise = pd.read_csv('C:\\Users\\css7c\\OneDrive\\Desktop\\Python_Programs\\Statistics_RegAnalysis_ML\\education_districtwise.csv')
+education_districtwise = education_districtwise.dropna()
+
