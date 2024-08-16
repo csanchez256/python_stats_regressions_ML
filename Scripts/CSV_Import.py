a215e5cd7d0ec78142c56feddc3e4a8c661437ee
chris <css7c3@gmail.com>
2024-08-16 15:43:29 -0600

    Max length + 256

--- Scripts/CSV_Import.py
+++ Scripts/CSV_Import.py
@@ -1,4 +1,6 @@
 
+
+
 import pandas as pd
 
 df = pd.read_csv('C:\\Users\\css7c\\OneDrive\\Desktop\\NM Paper Datasests 7.14.24\\SUMMARY_STATISTICS\\Data From Gabriel M 8.14.24\\data\\nm_medicaid_weighted_08_14_2024.csv')
@@ -7,7 +9,7 @@ alter_statements = []
 
 for column in df.columns:
     max_length = df[column].astype(str).str.len().max()
-    alter_statements.append(f"ALTER TABLE YourTableName ALTER COLUMN {column} VARCHAR({max_length + 10});") # Add some buffer
+    alter_statements.append(f"ALTER TABLE NM_Medicaid ALTER COLUMN {column} VARCHAR({max_length + 256});") # Add some buffer
 
 # Output the statements
 with open('alter_statements.sql', 'w') as f:
